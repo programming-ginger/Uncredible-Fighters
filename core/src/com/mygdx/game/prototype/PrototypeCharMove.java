44d155c12a540467bc36980d2f4c7a82b998caa8
Ignatius Juan Pradipta <ignatiusjuanpradipta@gmail.com>
2021-08-08 21:16:02 +0200

    add movement

---
+++ core/src/com/mygdx/game/prototype/PrototypeCharMove.java
@@ -0,0 +1,100 @@
+package com.mygdx.game.prototype;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+
+public class PrototypeCharMove extends ApplicationAdapter {
+    ShapeRenderer rectA;
+    ShapeRenderer rectB;
+
+    private final float defSpeed = 2;
+
+    private final float vpPaddingTop = 10;
+    private final float vpPaddingBottom = 30;
+    private final float vpPaddingLeft = 10;
+    private final float vpPaddingRight = 10;
+
+    private final float rectWidth = 50;
+    private final float rectHeight = 50;
+
+    float rectXA = (rectWidth/2) + vpPaddingLeft;
+    float rectYA = (rectHeight/2) + vpPaddingBottom;
+    float rectXB = (rectWidth/2) + vpPaddingLeft;
+    float rectYB = (rectHeight/2) + vpPaddingBottom;
+
+    float speedXA = 0;
+    float speedYA = 0;
+    float speedXB = 0;
+    float speedYB = 0;
+
+    @Override
+    public void create ()
+    {
+        rectA = new ShapeRenderer();
+        rectB = new ShapeRenderer();
+    }
+
+    @Override
+    public void render () {
+        speedXA = 0;
+        speedYA = 0;
+        speedXB = 0;
+        speedYB = 0;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.A))
+        {
+            if ((rectXA - speedXA) >= vpPaddingLeft + (rectWidth/2))
+            {
+                speedXA = -1 * defSpeed;
+            }
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D))
+        {
+            if ((rectXA + speedXA) < Gdx.graphics.getWidth() - vpPaddingRight - rectWidth*1.5)
+            {
+                speedXA = 1 * defSpeed;
+            }
+        }
+
+        if (Gdx.input.isKeyPressed(Input.Keys.DPAD_LEFT))
+        {
+            if ((rectXB - speedXB) >= vpPaddingLeft + (rectWidth/2))
+            {
+                speedXB = -1 * defSpeed;
+            }
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DPAD_RIGHT))
+        {
+            if ((rectXB + speedXB) < Gdx.graphics.getWidth() - vpPaddingRight - rectWidth*1.5)
+            {
+                speedXB = 1 * defSpeed;
+            }
+        }
+
+        rectXA += speedXA;
+        rectYA += speedYA;
+        rectXB += speedXB;
+        rectYB += speedYB;
+
+        Gdx.gl.glClearColor(.25f, .25f, .25f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        rectA.begin(ShapeRenderer.ShapeType.Filled);
+        rectA.setColor(1, 0, 0 , 1);
+        rectA.rect(rectXA, rectYA, rectWidth, rectHeight);
+        rectA.end();
+
+        rectB.begin(ShapeRenderer.ShapeType.Filled);
+        rectB.setColor(0, 1, 0 , 1);
+        rectB.rect(rectXB, rectYB, rectWidth, rectHeight);
+        rectB.end();
+    }
+
+    @Override
+    public void dispose () {
+        rectA.dispose();
+    }
+}
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,6 +3,7 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.*;
+import com.mygdx.game.prototype.PrototypeCharMove;
 
 public class DesktopLauncher {
 
@@ -19,5 +20,6 @@ public class DesktopLauncher {
 		Options.setWindowWidth(SCREEN_WIDTH);
 
 		new LwjglApplication(new UncredibleFighters(), config);
+		//new LwjglApplication(new PrototypeCharMove(), config);
 	}
 }
